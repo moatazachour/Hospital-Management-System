d7fefbb8a10519bd2210f27049dc2c438a984972
moatazachour <moatazachour84@gmail.com>
2025-01-28 22:59:04 +0100

    feat: Set up project architecture and database connection
    - Created project layers: Presentation, Business Logic, Data Access
    - Established database connection and configured connection string

---
+++ HMS_Data_Access_Layer/clsDataAccessSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS_Data_Access_Layer
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = ConfigurationManager.ConnectionStrings["DatabaseConnection"].ConnectionString;
+    }
+}
